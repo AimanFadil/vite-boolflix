8858ac51c9c12a5fa7e24cd5a34674a8fa7ba538
AimanFadil <aimanbello2001@gmail.com>
2024-01-12 14:29:18 +0100

    Milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
+<script>
+import { store } from './store.js';
+export default {
+  components: {
 
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss" >
+@use './styles/generals.scss';
+</style>
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    endpoint: 'https://api.themoviedb.org/3/search/movie?api_key=9d8a145937f8aeaaeb44276b273ddd7f&query=',
+    films: [],
+    film: ''
+})
